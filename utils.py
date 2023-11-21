079f76d1c0c80844bf589288e84b38ecc50a5cd4
KimRass <rmx1000@yonsei.ac.kr>
2023-11-21 14:59:35 +0900

    FANnet architecture

--- utils.py
+++ utils.py
@@ -17,8 +17,7 @@ import re
 ROOT = Path(__file__).resolve().parent
 FANNET_DIR = ROOT/"dataset/fannet"
 
-# CHARS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-CHARS = "0123456789"
+CHARS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 ASCII2IDX = {ord(char): idx for idx, char in enumerate(CHARS)}
 IDX2ASCII = {v: k for k, v in ASCII2IDX.items()}
 N_CLASSES = len(ASCII2IDX.values())
